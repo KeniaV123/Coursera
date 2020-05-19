3eb7d1e7ba8b03a1d723dd8e4fa09363ffba40fa
Kenia Villa <kenchi_kenia@hotmail.com>
2020-05-18 22:46:52 -0500

    agrego tarea 4

---
+++ module4-solution/script4.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Yaakov", "John","Frank", "Larry", "Jen", "Joe", "Paul",  "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+     } 
+})();
